5c38ce65a488333e9959c8fa56f2f6c0c8ad7236
Zhenwei Liu <lzhenwei76@gmail.com>
2026-07-14 21:39:29 +0200

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -9,32 +9,32 @@ return {
         Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/admin.lua",
         Description = "Fly, Noclip, ESP, Godmode, Touchfling"
     },
-    ["17664219670"] = {
+    [17664219670] = {
         Name = "Airstrikes NPCs",
         Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/GamepassFree.lua",
         Description = "Gamepasses for free, infinite jump"
     },
-    ["189707"] = {
+    [189707] = {
         Name = "Natural Disaster Survival",
         Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/Natural-Disaster-Survival.lua",
         Description = "Fly, Noclip, ESP, Godmode"
     },
-    ["174238106"] = {
+    [174238106] = {
         Name = "RED VS BLUE PLANE WARS",
         Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/RedVSBlue-PlaneWars.lua",
         Description = "Auto farm, Killall, ESP, Serverhop"
     },
-    ["4325400580"] = {
+    [4325400580] = {
         Name = "Army Tycoon",
         Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/Army-Tycoon.lua",
         Description = "Kill All Units, Get All Buildings, Lag Players, Nuke to Mouse (Q), Give All Players Buildings"
     },
-    ["128784467030899"] = {
+    [128784467030899] = {
         Name = "FIXED! Merge a Nuke Script",
         Script = "https://github.com/gumanba/Scripts/raw/refs/heads/main/MergeaNuke",
         Description = "Auto Merge, Auto Buy Upgrades, Anti Cheat Bypass"
     },
-    ["654732683"] = {
+    [654732683] = {
         Name = "Car Crushers 2",
         Script = "https://github.com/lzhenweiDev/NebulaHub/raw/refs/heads/main/Scripts/CarCrushers2.lua",
         Description = "Car Speed Hack, Auto Farm Money, Troll Players"
